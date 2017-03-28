982a9e878db8dab5fcff43f781fe73ae5317c443
FullStackDevoloper <code.apple.freak@gmail.com>
2017-03-28 07:27:19 +0200

    Create includes.h

---
+++ includes.h
@@ -0,0 +1,25 @@
+/*
+* includes.h
+*
+*  Created on: Mar 20, 2017
+*      Author: Mustafa
+*/
+
+#ifndef INCLUDES_H_
+#define INCLUDES_H_
+
+class Link;
+class Node;
+class List;
+
+#include "Link.h"
+#include "node.h"
+#include "List.h"
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+#endif /* INCLUDES_H_ */
