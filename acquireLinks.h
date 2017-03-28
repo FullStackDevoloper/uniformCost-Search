f7d3a25454f272a98e61b025b7473857eebc7256
FullStackDevoloper <code.apple.freak@gmail.com>
2017-03-28 07:28:35 +0200

    Create acquireLinks.h

---
+++ acquireLinks.h
@@ -0,0 +1,19 @@
+/*
+* acquireLinks.h
+*
+*  Created on: Mar 19, 2017
+*      Author: Mustafa
+*/
+
+#ifndef ACQUIRELINKS_H_
+#define ACQUIRELINKS_H_
+
+
+#include "includes.h"
+
+
+using namespace std;
+
+void acquireLinks1(vector<Link> &links);
+
+#endif /* ACQUIRELINKS_H_ */
