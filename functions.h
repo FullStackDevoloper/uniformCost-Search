a21de507872ce36b6609c19cd353d05040ff07be
FullStackDevoloper <code.apple.freak@gmail.com>
2017-03-28 07:27:42 +0200

    Create functions.h

---
+++ functions.h
@@ -0,0 +1,22 @@
+/*
+* functions.h
+*
+*  Created on: Mar 18, 2017
+*      Author: Mustafa
+*/
+
+#ifndef FUNCTIONS_H_
+#define FUNCTIONS_H_
+
+#include "includes.h"
+
+//#include <iostream>
+using namespace std;
+//#include <vector>
+//#include "Link.h"
+
+
+void acquireLinks(vector<Link> &links);
+
+
+#endif /* FUNCTIONS_H_ */
